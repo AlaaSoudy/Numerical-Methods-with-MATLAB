0146434922e65eca6af9cbf9ffec986dcb73920b
Alaa Soudy <154088063+AlaaSoudy@users.noreply.github.com>
2025-11-21 15:34:24 +0200

    Fix formatting and spelling errors in iteration script

--- fixed_point_iteration.m
+++ fixed_point_iteration.m
@@ -3,8 +3,8 @@
 g= input('Enter your function g(x): ','s');
 g= str2func(['@(x) ' g]); 
 x0= input('Enter initial value: ');
-e=input('Enter tolerence: ');
-n=input('Number of itteration: ');
+e=input('Enter tolerance: ');
+n=input('Number of iterations: ');
 converged = false; 
    fprintf('%-6s %-16s %-16s %-16s\n','Iter','xᵢ₋₁','xᵢ','Error');
 fprintf('%-6d %-16.10f %-16.10f %-16.10f\n',0,0,x0,abs(x0-0));
@@ -22,4 +22,5 @@ end
 
 if ~converged
     fprintf('\nMethod did not converge within %d iterations.\n',n);
+
 end
